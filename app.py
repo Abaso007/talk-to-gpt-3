93749ef64dccacf7d5daaed04148ea64cb8e0789
Sourcery AI <>
2023-04-19 04:47:23 +0000

    'Refactored by Sourcery'

--- app.py
+++ app.py
@@ -31,32 +31,34 @@ while True:
         with open('output.wav', 'wb') as f:
             f.write(audio.get_wav_data())
     print("no longer listening.\n")
-    
+
     try:
         output = model.transcribe('output.wav', fp16=False)
         user_input = output['text']
-        print("user input : " + user_input)
+        print(f"user input : {user_input}")
     except Exception as e :
         print(e)
         continue
-    
-    prompt = user_name + ": " + user_input + "\n" + bot_name+ ": "
-    
+
+    prompt = f"{user_name}: {user_input}" + "\n" + bot_name + ": "
+
     conversation += prompt
-    
+
     response = openai.Completion.create(
-            engine = "text-davinci-003",
-            prompt = conversation,
-            max_tokens = 1024,
-            n = 1,
-            stop=[user_name+":"],
-            temperature=0.2,
-        )
+        engine="text-davinci-003",
+        prompt=conversation,
+        max_tokens=1024,
+        n=1,
+        stop=[f"{user_name}:"],
+        temperature=0.2,
+    )
     response_str = response["choices"][0]["text"].replace("\n", "")
-    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
-    
+    response_str = response_str.split(f"{user_name}: ", 1)[0].split(
+        f"{bot_name}: ", 1
+    )[0]
+
     conversation += response_str + "\n"
     print(response_str)
-    
+
     engine.say(response_str)
     engine.runAndWait()
